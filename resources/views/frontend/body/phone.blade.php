b8ca4692c5fbca01f84e01f9fa5a16653736ed23
Ahmad Merie <ahbmer3i@gmail.com>
2022-08-18 15:14:45 +0300

    Fix Phone Issue

--- resources/views/frontend/body/phone.blade.php
+++ resources/views/frontend/body/phone.blade.php
@@ -2,13 +2,13 @@
     @php
 
 
-        $phone = \App\Models\ContactPhoneList::all();
+        $phone = \App\Models\ContactPhoneList::first();
 
         $phoneSection = \App\Models\Phone::findOrFail(1);
     @endphp
     <p>{{$phoneSection->subtitle}}</p>
     <h6 class="text-uppercase">{{$phoneSection->title}}</h6>
-    <h4><span style="color: #db4041;">{{!empty($phone->phone) ? $phone->phone->first() : ''}}</span>
+    <h4><span style="color: #db4041;">{{!empty($phone->phone) ? $phone->phone : ''}}</span>
     </h4></div>
 <div class="col-md-4 text-right"><img
         alt="finance2-pic3"
