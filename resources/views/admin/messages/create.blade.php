8a05f78872ba04c0b9a55b0e31efb52af525c58a
Ahmad Merie <ahbmer3i@gmail.com>
2023-02-06 14:59:18 +0300

    Fix PDF Attachment

--- resources/views/admin/messages/create.blade.php
+++ resources/views/admin/messages/create.blade.php
@@ -59,6 +59,62 @@
                                 <!-- end row -->
                             </div>
                         </div>
+                        <div class="card">
+                            <div class="card-body">
+
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div
+                                            class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                            <h4 class="mb-sm-0">Message Templates</h4>
+                                            <div class="page-title-right">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <form action="{{ route('messages.send') }}" method="post">
+                                    @csrf
+                                    <input type="hidden" name="id" value="">
+                                    <div class="row mb-3 mt-5">
+                                        <label for="mobile" class="col-sm-2 col-form-label">Mobile #</label>
+                                        <div class="col-sm-10">
+                                            <input class="form-control" type="text" id="mobile" name="mobile"
+                                                   placeholder="7xxxxxxxx"
+                                                   value="">
+                                            <span class="text-info">7xxxxxxxx</span>
+                                        </div>
+                                    </div>
+                                    <div class="row mb-3 mt-5">
+                                        <div class="col-sm-10">
+                                            <button
+                                                class="btn btn-info"
+                                                type="submit"
+                                            >Payment
+                                            </button>
+                                            <div class="col-sm-10">
+                                                <button
+                                                    class="btn btn-info"
+                                                    type="submit"
+                                                >Payment
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                        {{-- <div class="row mb-3 mt-5">
+                                             <label for="title" class="col-sm-2 col-form-label">Title</label>
+                                             <div class="col-sm-10">
+                                                 <textarea class="form-control" type="text" id="elm1"
+                                                           name="title"></textarea>
+                                             </div>
+                                         </div>--}}
+                                        <div class="pb-4"></div>
+                                        <input type="submit" class="btn btn-info waves-effect waves-light"
+                                               value="Send Message">
+                                </form>
+                                <!-- end row -->
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
