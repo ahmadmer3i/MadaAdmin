a899e74ccce0e3588f1335a4d2acbe6edaa52853
Ahmad Merie <ahbmer3i@gmail.com>
2023-06-04 19:55:34 +0300

    Fix Messages

--- app/Http/Controllers/API/FormApplicantController.php
+++ app/Http/Controllers/API/FormApplicantController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\ApplyForm;
+use http\Env\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FormApplicantController extends Controller
 {
     public function index(): JsonResponse
     {
+        if (Auth::check()) {
+            return response()->json('true');
+        } else {
+            return response()->json('true');
+        }
+
         $applicants = ApplyForm::all();
         return response()->json($applicants);
     }
