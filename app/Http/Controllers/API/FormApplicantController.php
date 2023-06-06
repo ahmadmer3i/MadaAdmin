66c64ee35de8285155435810eb008a541d698e57
Ahmad Merie <ahbmer3i@gmail.com>
2023-06-07 01:20:55 +0300

    Fix API

--- app/Http/Controllers/API/FormApplicantController.php
+++ app/Http/Controllers/API/FormApplicantController.php
@@ -20,7 +20,7 @@ class FormApplicantController extends Controller
             ->with('sponsor_bank')
             ->with('form_qualification')
             ->with('form_material_status')
-            ->with('transfer_ways')->latest()->take(50);
+            ->with('transfer_ways')->latest()->take(30);
         return response()->json($applicants);
     }
 }
